b7b8c2755b2b524ee121c3d9657a8367dd65254e
Kinoki445 <Kinoki445@gmail.com>
2024-03-30 19:15:43 +0500

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -94,3 +94,10 @@ closeButton3.addEventListener("click", function() { // Добавлено
 });
 
 window.addEventListener("click", windowOnClick);
+
+$(function () {
+    $(".menu__link, .footer__link").on("click", function (e) {e.preventDefault()
+    var id = $(this).attr('href')
+    var top = $(id).offset().top
+    $('body,html').animate({ scrollTop: top }, 1000)})
+});
